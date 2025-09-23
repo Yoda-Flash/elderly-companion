d09d5929cb8bea5a9c042b9fa2ff6bd3cfc99156
Yoda-Flash <kaitlyn.zee@gmail.com>
2025-09-23 13:31:28 -0700

    Created pipe, both with input and without input.py sections depending on where I'm coding

---
+++ __main__.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+import subprocess
+
+# input_process = subprocess.Popen(
+#     ['venv/Scripts/python.exe', '-u', 'input.py'], stdout=subprocess.PIPE
+# )
+
+# llm_process = subprocess.Popen(
+#     ['node', 'llm.js'], stdin=input_process.stdout, stdout=subprocess.PIPE
+# )
+
+llm_process = subprocess.Popen(
+    ['node', 'llm.js'], stdout=subprocess.PIPE
+)
+
+output_process = subprocess.Popen(
+    ['venv/Scripts/python.exe', '-u', 'output.py'], stdin=llm_process.stdout
+)
+
+llm_process.stdout.close()
+output_process.communicate()
